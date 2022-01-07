0de1bd59446dede56925ff34008b63f6d6f9e098
payal <pkatre93@gmail.com>
2022-01-07 23:14:02 +0530

    Adding inote  react project

---
+++ react/inotebook/backend/index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require("./db");
+const express = require("express");
+connectToMongo();
+const app = express();
+const port = 3000;
+
+//middleware - when yoy want to work with req.body
+app.use(express.json());
+
+//Available route
+app.use("/api/auth", require("./routes/auth"));
+//app.use("/api/notes", require("./routes/notes"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ react/inotebook/backend/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+
+//Create a user using : POST "/api/auth". Doesnt require auth
+router.get("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
+});
+module.exports = router;
